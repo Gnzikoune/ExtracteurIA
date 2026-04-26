2a67d224e794dd3580fc5bf10609475d7a32f157
Gnzikoune <gnzikoune@gmail.com>
2026-04-26 14:25:17 +0100

    mise à jour analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { ErrorBoundary } from './ErrorBoundary.tsx';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
